8a124f28f571316d920fe5713a29fa288c4f068c
jsn0509 <jsn_@naver.com>
2025-03-15 07:03:29 +0900

    feat: 아이콘 추가

---
+++ renderer/src/components/icons/index.ts
@@ -0,0 +1,7 @@
+export { ArrowRight } from './ArrowRight';
+export { Close } from './Close';
+export { ChevronLeft } from './ChevronLeft';
+export { CaretDown } from './CaretDown';
+export { CaretUp } from './CaretUp';
+export { Check } from './Check';
+export { Discord } from './Discord';
