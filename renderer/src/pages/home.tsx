9d2b5fad2c3cbefb6655287354ffb3a3575844ac
jsn0509 <jsn_@naver.com>
2025-03-14 18:26:39 +0900

    refactor: 페이지 정리, 레이아웃 추가

---
+++ renderer/src/pages/home.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HomePage() {
+  return <div className="mt-1 w-full flex-wrap flex justify-center"></div>;
+}
---
+++ renderer/src/pages/layout.tsx
@@ -0,0 +1,9 @@
+import { cn } from '@/lib/cn';
+
+export function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div className={cn('antialiased bg-background w-screen h-screen')}>{children}</div>;
+}
