7cead5d7738d3f6e9e3f561365e715e8676f9e69
jsn0509 <jsn_@naver.com>
2025-03-17 04:40:06 +0900

    refactor: IconButton 버튼 props 추가

--- renderer/src/components/ui/IconButton.tsx
+++ renderer/src/components/ui/IconButton.tsx
@@ -1,22 +1,22 @@
 import { cn } from '@/lib/cn';
 import * as icons from '@/components/icons';
+import { ButtonHTMLAttributes } from 'react';
 
-interface IconButtonProps {
+interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon: keyof typeof icons;
   className?: string;
-  onClick?: () => void;
 }
 
-export const IconButton = ({ className, onClick, icon }: IconButtonProps) => {
+export const IconButton = ({ className, icon, ...props }: IconButtonProps) => {
   const Icon = icons[icon];
 
   return (
     <button
       className={cn(
-        'size-7 rounded-sm flex items-center justify-center transition-colors duration-200 hover:bg-surface',
+        'size-7 rounded-sm flex items-center justify-center transition-colors duration-200 hover:bg-surface cursor-pointer',
         className,
       )}
-      onClick={onClick}
+      {...props}
     >
       <Icon />
     </button>
