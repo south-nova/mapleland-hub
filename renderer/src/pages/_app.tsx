e30425b9da955636385359caf6aaa450900dfc47
jsn0509 <jsn_@naver.com>
2025-03-17 04:46:33 +0900

    refactor: 툴팁 프로바이더 추가

--- renderer/src/pages/_app.tsx
+++ renderer/src/pages/_app.tsx
@@ -4,15 +4,18 @@ import { Layout } from '@/pages/layout';
 
 import '../styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { TooltipProvider } from '@/components/ui/ToolTip';
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <TooltipProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
