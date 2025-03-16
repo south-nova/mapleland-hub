e76ee2610774d7b4d1d0a4c00dfb8d02db418328
jsn0509 <jsn_@naver.com>
2025-03-17 04:41:30 +0900

    refactor: 뒤로가기 버튼 개선, 숨기기 버튼 추가

--- renderer/src/components/Title.tsx
+++ renderer/src/components/Title.tsx
@@ -1,17 +1,30 @@
 import { IconButton } from '@/components/ui/IconButton';
 import { useRouter } from 'next/router';
 
-export const Title = () => {
+interface TitleProps {
+  back?: boolean;
+}
+
+export const Title = ({ back = false }: TitleProps) => {
   const router = useRouter();
+
+  const handleHide = () => window.ipc.hide();
+
+  const handleClose = () => window.ipc.close();
 
   return (
     <div className="h-11 w-full flex items-center relative draggable">
-      <IconButton
-        className="absolute left-2 no-drag"
-        icon="ChevronLeft"
-        onClick={() => router.back()}
-      />
-      <IconButton className="absolute right-2 no-drag" icon="Close" />
+      {back && (
+        <IconButton
+          className="absolute left-2 no-drag"
+          icon="ChevronLeft"
+          onClick={() => router.back()}
+        />
+      )}
+      <div className="flex gap-0.5 absolute right-2 no-drag">
+        <IconButton icon="Hide" onClick={handleHide} />
+        <IconButton icon="Close" onClick={handleClose} />
+      </div>
     </div>
   );
 };
